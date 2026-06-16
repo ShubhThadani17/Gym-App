a6f9aaab35033b38268b6e7e43cdbcc9311511c0
Shubh Thadani <shubhthadani@gmail.com>
2026-06-16 19:58:37 +0530

    added get_current_user

--- core/auth.py
+++ core/auth.py
@@ -3,7 +3,12 @@
 from sqlalchemy.orm import Session
 from database.models import User
 from core.security import verify_password
-from fastapi import HTTPException , status
+from fastapi import HTTPException , status , Depends
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt , JWTError
+from core.config import settings
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def authenticate_user(db: Session, email: str, password: str):
     user=db.query(User).filter(User.email==email).first()
@@ -15,5 +20,17 @@ def authenticate_user(db: Session, email: str, password: str):
     
     return user
 
-def get_current_user():
-    pass
+def get_current_user(db : Session , token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Could not validate credentials")
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    
+    user = db.query(User).filter(User.email == email).first()
+    if user is None:
+        raise credentials_exception
+    return user
