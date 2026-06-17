c2d0c7b982d87051b9707b69af13764f2cd8b223
Shubh Thadani <shubhthadani@gmail.com>
2026-06-17 18:15:28 +0530

    updating get user

--- core/auth.py
+++ core/auth.py
@@ -7,6 +7,7 @@ from fastapi import HTTPException , status , Depends
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt , JWTError
 from core.config import settings
+from database.db import get_db
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -20,7 +21,7 @@ def authenticate_user(db: Session, email: str, password: str):
     
     return user
 
-def get_current_user(db : Session , token: str = Depends(oauth2_scheme)):
+def get_current_user(db : Session = Depends(get_db) , token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Could not validate credentials")
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
