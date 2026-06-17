91e4d3ac5743264ce7f8c76d025f869ee092dfae
Shubh Thadani <shubhthadani@gmail.com>
2026-06-17 18:16:29 +0530

    added new models

--- database/models.py
+++ database/models.py
@@ -2,6 +2,7 @@
 
 from sqlalchemy import Column , Integer , String , Date , ForeignKey
 from sqlalchemy.orm import declarative_base, relationship 
+from datetime import date
 
 Base=declarative_base()
 
@@ -9,7 +10,6 @@ class User(Base):
     __tablename__ = "users"
 
     id=Column(Integer , primary_key=True , index=True)
-    username=Column(String , unique=True , index=True)
     email=Column(String , unique=True , index=True)
     hashed_password=Column(String)
     members = relationship("Member", back_populates="user")
@@ -23,7 +23,32 @@ class Member(Base):
     phone=Column(String , unique=True , index=True)
     age=Column(Integer)
     gender=Column(String)
-    created_at=Column(Date)
+    created_at=Column(Date, default=date.today)
 
     user_id = Column(Integer, ForeignKey("users.id"))
     user = relationship("User", back_populates="members")
+    subscription = relationship("Subscription",back_populates="member",uselist=False)
+    payments = relationship("Payment",back_populates="member")
+
+class Subscription(Base):
+    __tablename__ = "subscriptions"
+
+    id=Column(Integer , primary_key=True , index=True)
+    member_id=Column(Integer , ForeignKey("members.id"))
+    start_date=Column(Date)
+    end_date=Column(Date)
+    status=Column(String)
+
+    member = relationship("Member", back_populates="subscription")
+
+class Payment(Base):
+    __tablename__ = "payments"
+
+    id=Column(Integer , primary_key=True , index=True)
+    member_id=Column(Integer , ForeignKey("members.id"))
+    amount=Column(Integer)
+    payment_date=Column(Date, default=date.today)
+    payment_method=Column(String)
+    status=Column(String)
+
+    member = relationship("Member",back_populates="payments")
