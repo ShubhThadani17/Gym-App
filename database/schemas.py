3833173dd34b3b35f57daedf8f42133d5d973fa3
Shubh Thadani <shubhthadani@gmail.com>
2026-06-18 12:33:41 +0530

    updated schemas

--- database/schemas.py
+++ database/schemas.py
@@ -1,6 +1,8 @@
 #Defines API request/response structures.
 
 from pydantic import BaseModel
+from datetime import date
+from typing import Optional
 
 class UserRegister(BaseModel):
 
@@ -13,3 +15,34 @@ class MemberCreate(BaseModel):
     phone:str
     age:int
     gender:str
+
+class MemberUpdate(BaseModel):
+    name: Optional[str] = None
+    email: Optional[str] = None
+    phone: Optional[str] = None
+    age: Optional[int] = None
+    gender: Optional[str] = None
+
+class SubscriptionCreate(BaseModel):
+    member_id:int
+    start_date:date
+    end_date:date
+    status:str
+
+class SubscriptionUpdate(BaseModel):
+    member_id:Optional[int] = None
+    start_date:Optional[date] = None
+    end_date:Optional[date] = None
+    status:Optional[str] = None
+
+class PaymentCreate(BaseModel):
+    member_id:int 
+    amount : float
+    payment_method : str
+    status: str = "pending"
+
+class DashboardResponse(BaseModel):
+    total_members: int
+    active_subscriptions: int
+    expired_subscriptions: int
+    total_revenue: float
