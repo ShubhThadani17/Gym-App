f0be769364a26a99f6f46c2fa8282f131da67490
Shubh Thadani <shubhthadani@gmail.com>
2026-06-21 01:17:54 +0530

    updates in member service functions

--- services/member_service.py
+++ services/member_service.py
@@ -3,25 +3,29 @@
 from sqlalchemy.orm import Session
 from database.models import Member
 from fastapi import HTTPException , status
+from sqlalchemy import func
 
-def create_member(db : Session , member_data):
-    new_member = Member(**member_data)
+def create_member(db : Session , member_data, user_id : int):
+    new_member = Member(**member_data.model_dump(),user_id=user_id)
     db.add(new_member)
     db.commit()
     db.refresh(new_member)
     return new_member
 
 
-def get_member(db :Session , member_id : int):
-    return db.query(Member).filter(Member.id == member_id).first()
+def get_member(db :Session , member_id : int , user_id: int):
+    return db.query(Member).filter(Member.id == member_id, Member.user_id == user_id).first()
 
 
-def get_all_members(db : Session):
-    return db.query(Member).all()
+def get_all_members(db : Session, user_id:int):
+     return (db.query(Member).filter(Member.user_id == user_id).all())
+
+def search_member(db : Session, user_id:int , name : str):
+    return (db.query(Member).filter(Member.user_id == user_id,func.lower(Member.name).contains(name.lower())).all())
 
 
-def update_member(db:Session , member_id:int , new_data):
-    member = db.query(Member).filter(Member.id==member_id).first()
+def update_member(db:Session , member_id:int , new_data , user_id:int):
+    member = db.query(Member).filter(Member.id==member_id, Member.user_id == user_id).first()
     if not member :
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Member not found")
     
@@ -32,9 +36,10 @@ def update_member(db:Session , member_id:int , new_data):
     return member
 
 
-def delete_member(db:Session , member_id:int):
-    member = db.query(Member).filter(Member.id==member_id).first()
+def delete_member(db:Session , member_id:int , user_id:int):
+    member = db.query(Member).filter(Member.id==member_id,Member.user_id == user_id).first()
     if not member :
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Member not found")
     db.delete(member)
     db.commit()
+    return {"message": "Member deleted successfully"}
