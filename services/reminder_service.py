697d747901f56645c7926a4660e9df4bdcf91d0d
Shubh Thadani <shubhthadani@gmail.com>
2026-06-22 01:12:56 +0530

    added reminder service

---
+++ services/reminder_service.py
@@ -0,0 +1,10 @@
+
+from datetime import date, timedelta
+from database.models import Subscription, Member
+
+
+def get_expiring_subscriptions(db):
+
+    tomorrow = date.today() + timedelta(days=1)
+
+    return (db.query(Subscription).join(Member).filter(Subscription.end_date == tomorrow).all())
