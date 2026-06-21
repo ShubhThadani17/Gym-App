603faba4d03d1787810036dffb0017ea1924e9ec
Shubh Thadani <shubhthadani@gmail.com>
2026-06-22 01:14:01 +0530

    integrated all the routers

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from routers.auth import router as auth_router
 from routers.members import router as members_router
+from routers.dashboard import router as dashboard_router
+from routers.subscriptions import router as subscription_router
+from routers.payments import router as payments_router
+from scheduler import scheduler
 
-from database.db import engine
-from database.models import Base
 
-app = FastAPI(title="Gym Management API")
 
-Base.metadata.create_all(bind=engine)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    scheduler.start()
+    yield
+    scheduler.shutdown()
+
+
+app = FastAPI(title="Gym Management API",lifespan=lifespan)
+
 
 app.include_router(auth_router,prefix="/auth",tags=["Authentication"])
 
-app.include_router(members_router,prefix="/members",tags=["Members"])
+app.include_router(dashboard_router,tags=["Dashboard"])
+
+app.include_router(members_router,tags=["Members"])
+
+app.include_router(subscription_router,tags=["Subscriptions"])
+
+app.include_router(payments_router, tags=["Payments"])
 
 
 @app.get("/")
