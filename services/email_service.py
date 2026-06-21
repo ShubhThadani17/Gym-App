72e0aac55f194839ec2f95d2b354ffc0d01f985a
Shubh Thadani <shubhthadani@gmail.com>
2026-06-22 01:12:40 +0530

    added email service

---
+++ services/email_service.py
@@ -0,0 +1,33 @@
+
+import smtplib
+from email.message import EmailMessage
+
+from core.config import settings
+
+
+def send_expiry_email(to_email: str, member_name: str):
+
+    msg = EmailMessage()
+    msg["Subject"] = "Gym Subscription Expiring Tomorrow"
+    msg["From"] = settings.EMAIL_ADDRESS
+    msg["To"] = to_email
+
+    msg.set_content(
+        f"""
+Hello {member_name},
+
+Your gym subscription will expire tomorrow.
+
+Please renew your subscription to continue enjoying gym access.
+
+Thank You.
+"""
+    )
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(
+            settings.EMAIL_ADDRESS,
+            settings.EMAIL_PASSWORD
+        )
+        server.send_message(msg)
