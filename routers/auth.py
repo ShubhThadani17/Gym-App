9f00df4d85e25a2a831568fe43ce3c4ab87ea909
Shubh Thadani <shubhthadani@gmail.com>
2026-06-23 00:34:23 +0530

    removed dead code in login

--- routers/auth.py
+++ routers/auth.py
@@ -24,8 +24,6 @@ def register(user: UserRegister , db=Depends(get_db)):
 @router.post("/login")
 def login(user: UserRegister , db=Depends(get_db)):
     db_user=authenticate_user(db , user.email , user.password)
-    if not db_user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED , detail="Invalid Credentials")
     
     access_token = create_access_token(data={"sub": db_user.email})
     return {"access_token": access_token , "token_type":"bearer"}
