f4809b1b5e616c1731a0fdf45589bc440ae3ed78
Shubh Thadani <shubhthadani@gmail.com>
2026-06-23 11:42:06 +0530

    wrapped the SMTP block in a try and except

--- services/email_service.py
+++ services/email_service.py
@@ -1,9 +1,10 @@
 
 import smtplib
+import logging
 from email.message import EmailMessage
-
 from core.config import settings
 
+logger = logging.getLogger(__name__)
 
 def send_expiry_email(to_email: str, member_name: str):
 
@@ -23,11 +24,10 @@ Please renew your subscription to continue enjoying gym access.
 Thank You.
 """
     )
-
-    with smtplib.SMTP("smtp.gmail.com", 587) as server:
-        server.starttls()
-        server.login(
-            settings.EMAIL_ADDRESS,
-            settings.EMAIL_PASSWORD
-        )
-        server.send_message(msg)
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(settings.EMAIL_ADDRESS, settings.EMAIL_PASSWORD)
+            server.send_message(msg)
+    except Exception as e:
+        logger.error(f"Failed to send email to {to_email}: {e}")
